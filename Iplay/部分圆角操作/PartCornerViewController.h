d9abc31bdc7c586654cdba768bd07e247b7e454d
Tuikai <510988176@qq.com>
2017-03-17 10:03:26 +0800

    控件使用UIBezierPath自定义切圆角

---
+++ Iplay/部分圆角操作/PartCornerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PartCornerViewController.h
+//  Iplay
+//
+//  Created by 推凯 on 2017/3/17.
+//  Copyright © 2017年 TK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PartCornerViewController : UIViewController
+
+@end
---
+++ Iplay/部分圆角操作/PartCornerViewController.m
@@ -0,0 +1,37 @@
+//
+//  PartCornerViewController.m
+//  Iplay
+//
+//  Created by 推凯 on 2017/3/17.
+//  Copyright © 2017年 TK. All rights reserved.
+//
+
+#import "PartCornerViewController.h"
+
+@interface PartCornerViewController ()
+
+@end
+
+@implementation PartCornerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
